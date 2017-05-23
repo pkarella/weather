19cc7dddf112a6799f0ff1a5b73ebf0517dfa902
David Shin and Peter Karellas <david+peter@>
2017-05-23 15:50:55 -0700

    completed functionality of APIs

---
+++ build/js/app.js
@@ -0,0 +1,22 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var apiKey = "990bbc0dfef73ea05ad6034184f72ec5";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey, function(response) {
+      console.log(response);
+      $('#text').text(response['weather'][0]['description']);
+      $('#text1').text(response['name']);
+      $('#text2').text('The temperature is '+(response['main']['temp']));
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+},{}]},{},[1]);
---
+++ tmp/allConcat.js
@@ -0,0 +1,19 @@
+var apiKey = "990bbc0dfef73ea05ad6034184f72ec5";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey, function(response) {
+      console.log(response);
+      $('#text').text(response['weather'][0]['description']);
+      $('#text1').text(response['name']);
+      $('#text2').text('The temperature is '+(response['main']['temp']));
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
